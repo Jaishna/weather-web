4f910cd9c112c6e4115f8cde67d60af9a9506c56
Jaishna <jaishna@Jaishnas-MacBook-Air.local>
2020-10-20 00:58:51 +0530

    j commit 1

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
+const https=require('https');
 var weather = require('weather-js');
 const geocode = require('./utils/geocode.js')
 const forecast = require('./utils/forecast.js')
+const ejs=require('ejs')
 const bodyParser = require("body-parser");
 const port = process.env.PORT || 3000
 const app = express()
@@ -18,12 +20,13 @@ app.use(express.static(public)) // express.static() middleware function to serve
 app.set('view engine', 'hbs')
 hbs.registerPartials(partials)
 
-app.get('/', (req, res) => {
-    res.render('index', {
-        title: 'Weather Channel',
-        name: 'Jaishna Singh Bhogal'
-    })
-})
+app.get('/', function (req, res){
+    // res.render('index', {
+    //     title: 'Weather Channel',
+    //     name: 'Jaishna Singh Bhogal'
+    // })
+    res.render("trial3");
+  })
 
 app.get('/about', (req, res) => {
     res.render('about', {
@@ -32,33 +35,41 @@ app.get('/about', (req, res) => {
     })
 })
 
-// app.get('/weather', (req, res) => {
-//     if(req.query.location === undefined | req.query.location === '') {
-//         return res.send({
-//             error: 'You did not enter any location!'
-//         })
-//     }
-//     const address = req.query.location
-//     geocode(address, (error, data) => {
-//         if(error) {
-//             res.send({error})
-//         } else {
-//             forecast(data.lattitude, data.longitude, (error, forecastData = {}) => {
-//                 if(error) {
-//                     res.send({error})
-//                 } else {
-//                     res.send({
-//                        place: data.location,
-//                         weather: forecastData,
-//                         location: req.query.location
-//                     })
-//                 }
-//             })
-//         }
-//     })
+app.post("/", function(req, res){
+  const city=(req.body.city);
+  const url="https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=06a5b16df8dc77f5bb49709bbb082159";
+
+  https.get(url, function(response)
+{
+  response.on("data", function(data){
+    const weatherData=JSON.parse(data);
+   const w=Math.round((weatherData.main.temp)-273)+" deg C";
+   const hum=(weatherData.main.humidity);
+   const des=(weatherData.weather[0].description);
+   const name=weatherData.name;
+   const pr=weatherData.main.pressure;
+     res.render("info", {temp:w, name:name, hum:hum, des:des, pr:pr});
+  })
+})
+
+})
 //
-// 
+// https.get(url, function(response){
+// const w;
+// response.on("data", function(data)
+// {
+//  const weatherData=JSON.parse(data);
+//  w=(weatherData.main.temp);
+// const hum=(weatherData.main.humidity);
+// const des=(weatherData.weather[0].description);
+// console.log(des);
 // })
+// })
+// app.get("/", function(req,res)
+// {
+//   res.render("info",{temp:w})
+// })
+
 
 
 app.get('/*', (req, res) => {
